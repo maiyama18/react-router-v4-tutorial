d05cac0e68210157b5a9e7ade1943f6a4e4e5bcc
hoture <myam0118@gmail.com>
2017-06-26 20:13:17 +0900

    init

---
+++ src/App.js
@@ -0,0 +1,129 @@
+import React, { Component } from 'react'
+import { BrowserRouter, Route, Link } from 'react-router-dom'
+
+const App = () => (
+  <BrowserRouter>
+    <div>
+      <ul>
+        <li><Link to='/'>Home</Link></li>
+        <li><Link to='/about'>About</Link></li>
+        <li><Link to='/friends'>Friends</Link></li>
+      </ul>
+
+      <hr />
+      <Route exact path='/' component={Home} />
+      <Route path='/about' component={About} />
+      <Route path='/friends' component={Friends} />
+    </div>
+  </BrowserRouter>
+)
+
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+    <p>Welcome to ようこそ</p>
+  </div>
+)
+
+const About = () => (
+  <div>
+    <h2>About</h2>
+    <p>フレンズに投票するページです</p>
+  </div>
+)
+
+class Friends extends Component {
+  constructor() {
+    super()
+    this.state = {}
+    this.handleVote = this.handleVote.bind(this)
+  }
+
+  componentWillMount() {
+    FRIENDS.forEach(friend => {
+      this.setState({
+        ...this.state,
+        [friend.id]: 0
+      })
+    })
+  }
+
+  handleVote(id) {
+    this.setState({
+      [id]: this.state[id] + 1
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Friends</h2>
+        <Route exact path='/friends' render={props => <FriendList handleVote={this.handleVote} />} />
+        <Route path='/friends/:id' render={props => <Friend match={props.match} votes={this.state} />} />
+      </div>
+    )
+  }
+}
+const FriendList = props => (
+  <div>
+    {FRIENDS.map(friend => (
+      <li>
+        <Link to={`/friends/${friend.id}`}>{friend.nameJa}</Link>
+        <button onClick={() => props.handleVote(friend.id)}>+</button>
+      </li>
+    ))}
+  </div>
+)
+
+const Friend = props => {
+  const { id } = props.match.params
+  const friend = friendById(id)
+  const vote = props.votes[id]
+
+  if (typeof friend === 'undefined')  {
+    return (
+      <div>
+        <p>Friends with id '{id}' does not exist.</p>
+      </div>
+    )
+  }
+
+  const containerStyle = { border: '1px gray solid', display: 'inline-block', padding: 10 }
+  const contentsStyle = { margin: 0 }
+
+  return (
+    <div>
+      <div style={containerStyle}>
+        <p style={contentsStyle}>{friend.family}</p>
+        <h1 style={contentsStyle}>{friend.nameJa}</h1>
+        <p style={contentsStyle}>{friend.nameEn}</p>
+      </div>
+      <h1>Vote: {vote}</h1>
+    </div>
+  )
+}
+
+const FRIENDS = [
+  {
+    id: 'serval',
+    nameJa: 'サーバル',
+    nameEn: 'Serval Cat',
+    family: 'ネコ目ネコ科ネコ属'
+  },
+  {
+    id: 'raccoon',
+    nameJa: 'アライグマ',
+    nameEn: 'Common raccoon',
+    family: 'ネコ目アライグマ科アライグマ属'
+  },
+  {
+    id: 'fennec',
+    nameJa: 'フェネック',
+    nameEn: 'Fennec',
+    family: 'ネコ目イヌ科キツネ属'
+  }
+]
+
+const friendById = id => FRIENDS.find(friend => friend.id === id)
+
+export default App
